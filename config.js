96a7d59cf8f6f9615fb6d5df8068986f9898660a
oanagrecu <oanagrecu09@yahoo.com>
2023-07-04 12:13:51 +0200

    refactored code

---
+++ config.js
@@ -0,0 +1,4 @@
+const Data = {
+  key: "899ebe2b8a35dfdf38dd465a71c3f9fa",
+};
+export default Data;
---
+++ script.js
@@ -0,0 +1,59 @@
+import Data from "./config.js";
+import { displayWeatherCards } from "./displayData.js";
+const searchBar = document.querySelector("#searchBar");
+const container = document.querySelector(".container");
+const cityNameContainer = document.querySelector(".city-name");
+
+const weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+// Event will start on a keyup action
+searchBar.addEventListener("keyup", getCoordonates);
+
+function getCoordonates(event) {
+  // checking the action for specific key (Enter)
+  if (event.key === "Enter") {
+    // Store target in variable
+    const thisCity = event.target.value.toLowerCase();
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${thisCity}&appid=${Data.key}`;
+    // Fetching first api to get the City coordinates
+    event.currentTarget.value = "";
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        cityNameContainer.innerHTML = data.name;
+        const lon = data.coord.lon;
+        console.log(lon);
+        const lat = data.coord.lat;
+        console.log(lat);
+
+        getData(lat, lon);
+      });
+  }
+}
+function getData(lat, lon) {
+  //Fetching final data according to the coordinates
+  fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt=5&units=metric&appid=${Data.key}`
+  )
+    .then((response) => response.json())
+    .then((result) => {
+      //   console.log(
+      //     "Welcome to this basic weather app. this is not a product but the product of an academic exercise."
+      //   );
+      console.log(result);
+      //   //  Removing all child elements from Container before creating new set of elements
+      while (container.firstChild) {
+        container.removeChild(container.firstChild);
+      }
+      displayWeatherCards(result, container, weekdays);
+    });
+}
